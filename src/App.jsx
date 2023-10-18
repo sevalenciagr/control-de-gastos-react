0af769e40ba235f64d7558af81fce64c34aa3210
Sebastián Valencia G <sevalenciagr@gmail.com>
2023-10-18 14:26:29 -0500

    Agregando total gastado

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 
 function App() {
+  
+  const [gastos, setGastos] = useState([]);
 
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
@@ -17,7 +19,6 @@ function App() {
   const [modal, setModal] = useState(false);
   const [animarModal, setAnimarModal] = useState(false);
 
-  const [gastos, setGastos] = useState([]);
 
   const handleNuevoGasto = () => {
     setModal(true);
@@ -41,8 +42,9 @@ function App() {
   };
 
   return (
-    <div className={ modal && 'fijar' }>
+    <div className={ modal ? 'fijar' : '' }>
       <Header 
+        gastos={gastos}
         presupuesto={presupuesto}
         setPresupuesto={setPresupuesto}
         isValidPresupuesto={isValidPresupuesto}
